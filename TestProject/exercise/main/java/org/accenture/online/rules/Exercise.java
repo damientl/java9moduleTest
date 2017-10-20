778b8a006db958fa0db17e9cd5b37ce12fe140f7
damientl <damien.tl@gmail.com>
2017-10-20 13:58:21 -0200

    ide recognize java module

--- TestProject/exercise/main/java/org/accenture/online/rules/Exercise.java
+++ TestProject/exercise/main/java/org/accenture/online/rules/Exercise.java
@@ -3,6 +3,6 @@ package org.accenture.online.rules;
 public class Exercise {
     public String hello() {
         Rules rule = new Rules();
-        return "hi";
+        return "hi Rules";
     }
 }
